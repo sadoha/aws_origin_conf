c255307193e301124e1e1c581e5abd7e170041b9
Andrey Sadovskiy <andrey.sadovskiy@dataart.com>
2019-09-24 15:15:54 +0300

    Current configuration has been updated

---
+++ dev/outputs.tf
@@ -0,0 +1,7 @@
+output "vpc_cidr" {
+  value = "${module.vpc.vpc_cidr}"
+}
+
+output "vpc_id" {
+  value = "${module.vpc.vpc_id}"
+}
--- modules/dev/vpc/outputs.tf
+++ modules/dev/vpc/outputs.tf
@@ -2,6 +2,6 @@ output "vpc_id" {
   value = "${aws_vpc.vpc.id}"
 }
 
-output "cidr_block" {
-  value = "${aws_vpc.vpc.cidr_block}"
+output "vpc_cidr" {
+  value = "${var.vpc_cidr}"
 }
