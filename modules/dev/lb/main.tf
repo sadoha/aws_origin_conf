b5057436fe1bce9f12ec0626b99929e491e25c21
Andrey Sadovskiy <andrey.sadovskiy@dataart.com>
2019-09-26 23:46:37 +0300

    Configeration has been updated.

--- modules/dev/lb/main.tf
+++ modules/dev/lb/main.tf
@@ -1,10 +1,10 @@
 resource "aws_lb" "lb" {
-  name                          = "alb-${var.env}"
-  security_groups               = ["${var.security_groups}"]
-  subnets                       = ["${var.subnet_public_az0}", "${var.subnet_public_az1}"]
-  internal                              = false
-  enable_cross_zone_load_balancing      = true
-  enable_deletion_protection            = true
+  name                          	= "alb-${var.env}"
+  security_groups               	= ["${var.security_groups}"]
+  subnets                       	= ["${var.subnet_public_az0}", "${var.subnet_public_az1}"]
+  internal                              = "false"
+  enable_cross_zone_load_balancing      = "true"
+  enable_deletion_protection            = "false"
 
   tags   = "${merge(map("Name", "lb-${var.env}-${var.name}"), var.tags)}"
 
