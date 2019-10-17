71cfe5b5e0d26cee938eb6422a7271fbb458211b
Andrey Sadovskiy <andrey.sadovskiy@dataart.com>
2019-10-17 13:31:13 +0300

    The scheme of infrastructure has been provided

--- modules/init/s3/main.tf
+++ modules/init/s3/main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "tslog_bucket" {
-  bucket = "s3-tslogs-bucket-${var.env}-${var.name}"
-  acl = "log-delivery-write"
+  bucket 		= "s3-tslogs-bucket-${var.env}-${var.name}"
+  acl 			= "log-delivery-write"
 
   versioning {
     enabled             = "true"
@@ -26,8 +26,8 @@ resource "aws_s3_bucket" "terraform_state" {
   }
 
   logging {
-    target_bucket = "${aws_s3_bucket.tslog_bucket.id}"
-    target_prefix = "log/"
+    target_bucket 	= "${aws_s3_bucket.tslog_bucket.id}"
+    target_prefix 	= "log/"
   }
 
   tags   = "${merge(map("Name", "s3-terraform-state-${var.env}-${var.name}"), var.tags)}"
